51b648d9435eb1b86d33f727e960b78faa5abe82
Alejandra Huaman <alejandrahuaman27@gmail.com>
2025-08-19 21:27:25 -0300

    Products.html + style.css + products.js (sin finalizar)

---
+++ js/products.js
@@ -0,0 +1,21 @@
+//19/08 parte 3 de entrega 1, revisar
+
+//qué constantes/let selecciono??
+
+/*fetch('https://japceibal.github.io/emercado-api/cats_products/101.json')
+    .then(res => {
+        if (!res.ok) {
+            throw new Error("El producto no se encuentra");
+        }
+        return res.json();
+    })
+    .then(data => {
+        const nombre = data.name;
+        const desc = data.description;
+        const cost = data.cost;
+        const currency = data.currency;
+        const soldout = data.soldout;
+        const img = data.image;
+//-----//
+    })
+*/
