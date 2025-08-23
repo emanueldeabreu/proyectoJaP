a634975100a8cc8373b326e24e36cdaac97d4f97
kiaraodimatteo <kiaraodimatteo@gmail.com>
2025-08-22 21:20:22 -0300

    Se agrega local storage
    Se agrega local storage

--- js/login.js
+++ js/login.js
@@ -1,13 +1,19 @@
-function mostrarAlerta(mensaje) {
-  let alerta = document.getElementById("alert-danger")
-  alerta.textContent = mensaje
-  alerta.classList.add("show")
-  
-  
-  setTimeout(() => {
-        alerta.classList.remove("show");
-    }, 5000);
+function cargarDatos() {
+  let sesionCheck = localStorage.getItem("sesionActiva");
 
+  if (sesionCheck === null) {
+    window.open("login.html", "_self");
+  }
+}
+
+function mostrarAlerta(mensaje) {
+  let alerta = document.getElementById("alert-danger");
+  alerta.textContent = mensaje;
+  alerta.classList.add("show");
+
+  setTimeout(() => {
+    alerta.classList.remove("show");
+  }, 5000);
 }
 
 document.getElementById("btn_login").addEventListener("click", function (e) {
@@ -15,16 +21,17 @@ document.getElementById("btn_login").addEventListener("click", function (e) {
   let usuario = document.getElementById("usuario").value;
   let contrasena = document.getElementById("contrasena").value;
 
-    if (usuario === "" && contrasena === "") {
+  if (usuario === "" && contrasena === "") {
     mostrarAlerta("Por favor no deje campos en blanco.");
-    }
-    if (usuario === "" && contrasena !== "") {
-      mostrarAlerta("Por favor introducir nombre de usuario.");
-    }
-    if (usuario !== "" && contrasena === "") {
-      mostrarAlerta("Por favor introducir contraseña.");
-    } 
-    if (usuario !== "" && contrasena !== "") {
-    window.open("index.html", "_self")
+  }
+  if (usuario === "" && contrasena !== "") {
+    mostrarAlerta("Por favor introducir nombre de usuario.");
+  }
+  if (usuario !== "" && contrasena === "") {
+    mostrarAlerta("Por favor introducir contraseña.");
+  }
+  if (usuario !== "" && contrasena !== "") {
+    window.open("index.html", "_self");
+    localStorage.setItem("sesionActiva", "si");
   }
 });
