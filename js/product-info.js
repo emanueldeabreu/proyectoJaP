0a87b468f4f849a7f69003b9e2af9ff252e0826d
emanueldeabreu <e.deabreu@outlook.com>
2025-10-05 17:56:07 -0300

    Sección comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -11,9 +11,16 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch(PRODUCT_INFO_URL)
   .then(response => response.json())
   .then(product => {
-    renderProductInfo(product);          // muestra el detalle del producto
-    renderRelatedProducts(product.id);   // 👈 muestra los productos relacionados
-  })
+  renderProductInfo(product); // muestra el detalle del producto
+
+  const COMMENTS_URL = `https://japceibal.github.io/emercado-api/products_comments/${product.id}.json`;
+
+  fetchComments(COMMENTS_URL)  //carga comentarios
+    .then(() => {
+      renderRelatedProducts(product.id); // productos relacionados luego de comentarios
+    });
+})
+
   .catch(error => {
     console.error("Error al obtener datos del producto:", error);
     document.querySelector("main").innerHTML = "<p>Error al cargar el producto.</p>";
@@ -132,6 +139,43 @@ function renderProductInfo(product) {
     });
   });
 }
+
+function fetchComments(url) {
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => renderCommentsSection(data))
+    .catch(error => console.error("Error al obtener comentarios:", error));
+}
+
+function renderCommentsSection(apiComments) {
+  const main = document.querySelector("main");
+
+  const commentsBox = document.createElement("div");
+  commentsBox.classList.add("product-card", "comments-box", "mt-4");
+  commentsBox.innerHTML = `
+    <h3>Comentarios</h3>
+    <div id="comments-container" class="mt-4"></div>
+  `;
+
+  main.insertAdjacentElement("beforeend", commentsBox);
+
+  const commentsContainer = commentsBox.querySelector("#comments-container");
+
+  commentsContainer.innerHTML = apiComments.map(c => `
+    <div class="comment-item border-top pt-2 mt-2">
+      <div class="d-flex justify-content-between align-items-center">
+        <strong>${c.user}</strong>
+        <small class="text-muted">${c.dateTime}</small>
+      </div>
+      <div class="text-warning">
+        ${"★".repeat(c.score)}${"☆".repeat(5 - c.score)}
+        <strong class="ms-2">(${c.score})</strong>
+      </div>
+      <p>${c.description}</p>
+    </div>
+  `).join("");
+}
+
 
 function renderRelatedProducts(currentId) {
   const catID = localStorage.getItem("catID");  // sacamos la categoría actual
